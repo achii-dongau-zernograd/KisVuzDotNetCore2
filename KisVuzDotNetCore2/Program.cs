240a246d7477339fb3f19cc6aabc174366fd456c
LitvinovVN <LitvinovVN@rambler.ru>
2018-03-21 11:44:12 +0300

    init user

--- KisVuzDotNetCore2/Program.cs
+++ KisVuzDotNetCore2/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using KisVuzDotNetCore2.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace KisVuzDotNetCore2
@@ -13,8 +15,25 @@ namespace KisVuzDotNetCore2
     public class Program
     {
         public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
+        {            
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {                    
+                    AppIdentityDBContext.CreateAdminAccount(services, services.GetService<IConfiguration>()).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
