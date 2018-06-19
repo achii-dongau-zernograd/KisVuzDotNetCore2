a3a04a65de3ccbdbcb85b5d392fe7b311c0ca166
LitvinovVN <LitvinovVN@rambler.ru>
2018-06-19 12:17:13 +0300

    RowStatuses init and menu

---
+++ KisVuzDotNetCore2/Models/InitDatabase/InitDatabaseRowStatuses.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KisVuzDotNetCore2.Models.InitDatabase
+{
+    /// <summary>
+    /// Инициализация таблицы "Статусы записей"
+    /// </summary>
+    public class InitDatabaseRowStatuses
+    {
+        /// <summary>
+        /// Инициализация таблицы "Статусы записей"
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static async Task CreateRowStatuses(IServiceProvider serviceProvider, IConfiguration configuration)
+        {
+            using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                AppIdentityDBContext context = serviceScope.ServiceProvider.GetService<AppIdentityDBContext>();
+
+                #region Инициализация таблицы "Статусы записей"
+                if (!await context.RowStatuses.AnyAsync())
+                {
+                    RowStatus RowStatus1 = new RowStatus
+                    {
+                        RowStatusId=1,
+                        RowStatusName="Ожидает подтверждения"
+                    };
+
+                    RowStatus RowStatus2 = new RowStatus
+                    {
+                        RowStatusId = 2,
+                        RowStatusName = "Подтверждено"
+                    };
+                    
+                    await context.RowStatuses.AddRangeAsync(
+                        RowStatus1,
+                        RowStatus2
+                    );
+                    await context.SaveChangesAsync();
+                }
+                #endregion
+            }
+        }
+    }
+}
