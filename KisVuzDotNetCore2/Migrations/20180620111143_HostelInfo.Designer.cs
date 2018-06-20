3418cc2ba245c4fcf54aee9b9234d93371380419
KaunOYu <okaun@mail.ru>
2018-06-20 14:16:41 +0300

    HostelInfo

--- KisVuzDotNetCore2/Migrations/20180620111143_HostelInfo.Designer.cs
+++ KisVuzDotNetCore2/Migrations/20180620111143_HostelInfo.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace KisVuzDotNetCore2.Migrations
 {
     [DbContext(typeof(AppIdentityDBContext))]
-    partial class AppIdentityDBContextModelSnapshot : ModelSnapshot
+    [Migration("20180620111143_HostelInfo")]
+    partial class HostelInfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
