afa3a82ea4096a354add1e708dc4a9c2ef4f9e71
LitvinovVN <LitvinovVN@rambler.ru>
2018-09-18 15:43:36 +0300

    kurator vedomosti

--- KisVuzDotNetCore2/Controllers/Students/VedomostiOfKuratorController.cs
+++ KisVuzDotNetCore2/Controllers/Students/VedomostiOfKuratorController.cs
@@ -64,6 +64,11 @@ namespace KisVuzDotNetCore2.Controllers
             return View(vedomost);
         }
 
+        /// <summary>
+        /// Сформировать список студентов с оценками для текущей ведомости
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<IActionResult> CreateVedomostStudentMarks(int id)
         {
             var vedomost = await _context.Vedomosti
@@ -86,7 +91,7 @@ namespace KisVuzDotNetCore2.Controllers
                 await _context.SaveChangesAsync();
             }
 
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(VedomostStudentMarks), new { id });
         }
 
         /// <summary>
@@ -112,7 +117,162 @@ namespace KisVuzDotNetCore2.Controllers
 
             return RedirectToAction(nameof(Index));
         }
-             
+
+        /// <summary>
+        /// Добавление строки в ведомость
+        /// </summary>
+        /// <param name="id">УИД ведомости</param>
+        /// <returns></returns>
+        public async Task<IActionResult> VedomostStudentMarksAddRow(int id)
+        {
+            // Определяем группу, для которой составлена ведомость
+            var studentGroup = await _context.Vedomosti.Where(v => v.VedomostId == id).SingleOrDefaultAsync();
+
+            // Составляем полный список студентов группы, для которой составлена ведомость
+            var students = await _context.Students.Where(s => s.StudentGroupId == studentGroup.StudentGroupId).ToListAsync();
+
+            // Получаем уже имеющиеся записи ведомости
+            var editingStudentMark = await _context.VedomostStudentMarks
+                .Include(m => m.VedomostStudentMarkName)
+                .Include(m => m.Student.StudentGroup.EduKurs)
+                .Include(m => m.Vedomost.EduYear)
+                .Include(m => m.Vedomost.SemestrName)
+                .Where(m => m.VedomostId == id).ToListAsync();
+
+            var addedStudentsIds = editingStudentMark.Select(m=>m.StudentId).ToList();
+
+            // Если уже имеются какие-либо записи,
+            // исключаем студентов из списка,
+            // чтобы избежать дублирования
+            if (editingStudentMark.Count>0)
+            {
+                foreach (var addedStudentsId in addedStudentsIds)
+                {
+                    var addedStudent = students.Where(s => s.StudentId == addedStudentsId).SingleOrDefault();
+                    if(addedStudent!=null)
+                    {
+                        students.Remove(addedStudent);
+                    }                    
+                }
+            }
+
+            if(students.Count>0)
+            {
+                ViewBag.students = new SelectList(students,
+                "StudentId",
+                "StudentFio");
+            }
+            
+            var vedomost = await _context.Vedomosti
+                .Include(v => v.EduYear)
+                .Include(v => v.SemestrName)
+                .Include(v => v.StudentGroup.EduKurs)
+                .Include(v => v.StudentGroup.Students)
+                .Include(v => v.VedomostStudentMarks)
+                    .ThenInclude(m => m.VedomostStudentMarkName)
+                .SingleOrDefaultAsync(v => v.VedomostId == id);
+            ViewBag.vedomost = vedomost;
+
+            if (vedomost == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.VedomostStudentMarkNameId = new SelectList(_context.VedomostStudentMarkNames,
+                "VedomostStudentMarkNameId",
+                "VedomostStudentMarkNameName");
+
+            var vedomostStudentMark = new VedomostStudentMark();
+            vedomostStudentMark.VedomostId = id;
+
+            return View(vedomostStudentMark);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> VedomostStudentMarksAddRow(VedomostStudentMark vedomostStudentMark)
+        {
+            _context.VedomostStudentMarks.Add(vedomostStudentMark);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(VedomostStudentMarks), new { id = vedomostStudentMark.VedomostId });
+        }
+
+        /// <summary>
+        /// Редактирование оценки студента в ведомости
+        /// </summary>
+        /// <param name="id">УИД ведомости</param>
+        /// <param name="StudentMarkId">УИД оценки студента в ведомости</param>
+        /// <returns></returns>
+        public async Task<IActionResult> VedomostStudentMarksEditRow(int id, int VedomostStudentMarkId)
+        {
+            var editingStudentMark = await _context.VedomostStudentMarks
+                .Include(m => m.VedomostStudentMarkName)
+                .Include(m => m.Student.StudentGroup.EduKurs)
+                .Include(m => m.Vedomost.EduYear)
+                .Include(m => m.Vedomost.SemestrName)
+                .Where(m => m.VedomostId == id && m.VedomostStudentMarkId == VedomostStudentMarkId)
+                .SingleOrDefaultAsync();
+
+            ViewBag.VedomostStudentMarkNameId = new SelectList(_context.VedomostStudentMarkNames,
+                "VedomostStudentMarkNameId",
+                "VedomostStudentMarkNameName",
+                editingStudentMark.VedomostStudentMarkNameId);
+
+            return View(editingStudentMark);
+        }
+                
+
+        [HttpPost]
+        public async Task<IActionResult> VedomostStudentMarksEditRow(VedomostStudentMark vedomostStudentMark)
+        {
+            var editingStudentMark = await _context.VedomostStudentMarks                
+                .Where(m => m.VedomostStudentMarkId == vedomostStudentMark.VedomostStudentMarkId)
+                .SingleOrDefaultAsync();
+
+            if(editingStudentMark!=null)
+            {
+                editingStudentMark.VedomostStudentMarkNameId = vedomostStudentMark.VedomostStudentMarkNameId;
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(VedomostStudentMarks), new { id = vedomostStudentMark.VedomostId });
+        }
+
+        /// <summary>
+        /// Удаление оценки студента в ведомости
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="VedomostStudentMarkId"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> VedomostStudentMarksDeleteRow(int id, int VedomostStudentMarkId)
+        {
+            var deletingStudentMark = await _context.VedomostStudentMarks
+                .Include(m => m.VedomostStudentMarkName)
+                .Include(m => m.Student.StudentGroup.EduKurs)
+                .Include(m => m.Vedomost.EduYear)
+                .Include(m => m.Vedomost.SemestrName)
+                .Where(m => m.VedomostId == id && m.VedomostStudentMarkId == VedomostStudentMarkId)
+                .SingleOrDefaultAsync();                        
+
+            return View(deletingStudentMark);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> VedomostStudentMarksDeleteRow(VedomostStudentMark vedomostStudentMark)
+        {
+            var deletingStudentMark = await _context.VedomostStudentMarks
+                .Where(m => m.VedomostStudentMarkId == vedomostStudentMark.VedomostStudentMarkId)
+                .SingleOrDefaultAsync();
+
+            if (deletingStudentMark != null)
+            {
+                _context.VedomostStudentMarks.Remove(deletingStudentMark);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(VedomostStudentMarks), new { id = vedomostStudentMark.VedomostId });
+        }
+
 
         // GET: Vedomosti/Edit/5
         public async Task<IActionResult> Edit(int? id)
