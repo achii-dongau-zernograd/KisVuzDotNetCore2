6cfac7dfa360bd6da68c0528fb073d2b57dfb9d1
LitvinovVN <LitvinovVN@rambler.ru>
2019-01-25 14:52:50 +0300

    courses sorting3

--- KisVuzDotNetCore2/Controllers/Users/RefresherCoursesController.cs
+++ KisVuzDotNetCore2/Controllers/Users/RefresherCoursesController.cs
@@ -48,7 +48,7 @@ namespace KisVuzDotNetCore2.Controllers.Users
                     .Include(r => r.AppUser)
                     .Include(r => r.RefresherCourseFile)
                     .Include(r => r.RowStatus)
-                    .OrderBy(r => r.AppUser.LastName);
+                    .OrderBy(r => r.RefresherCourseDateIssue);
                     ViewBag.Id = id;
                     return View(await userQualifications.ToListAsync());
                 }
@@ -61,7 +61,8 @@ namespace KisVuzDotNetCore2.Controllers.Users
                 var refresherCourses = _context.RefresherCourses
                 .Include(r => r.AppUser)
                 .Include(r => r.RefresherCourseFile)
-                .Include(r => r.RowStatus);
+                .Include(r => r.RowStatus)
+                .OrderBy(r => r.AppUser.GetFullName);
                 return View(await refresherCourses.ToListAsync());
             }
 
