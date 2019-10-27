f894b59157dbe0dba42cfb6ce104fb2909902920
LitvinovVN <LitvinovVN@rambler.ru>
2019-10-27 13:15:27 +0300

    Teachers. Work experience

---
+++ KisVuzDotNetCore2/Migrations/20191027094809_TeacherWorkExperience.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace KisVuzDotNetCore2.Migrations
+{
+    public partial class TeacherWorkExperience : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WorkExperienceGeneral",
+                table: "Teachers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WorkExperienceSpecialty",
+                table: "Teachers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WorkExperienceGeneral",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "WorkExperienceSpecialty",
+                table: "Teachers");
+        }
+    }
+}
