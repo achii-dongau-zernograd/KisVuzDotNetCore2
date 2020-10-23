df99d07378669caf0cbbd9ca532bb7a56e087956
LitvinovVN <LitvinovVN@rambler.ru>
2020-10-23 12:11:47 +0300

    t19 BaseNir

---
+++ KisVuzDotNetCore2/Migrations/20201023083832_BaseNir.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace KisVuzDotNetCore2.Migrations
+{
+    public partial class BaseNir : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BaseNir",
+                table: "EduNir",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BaseNir",
+                table: "EduNir");
+        }
+    }
+}
