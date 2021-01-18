ec6a9300685d8b42099eb273a8ddd6ff4e1f163d
LitvinovVN <LitvinovVN@rambler.ru>
2021-01-18 17:05:38 +0300

    ElGradebook

---
+++ KisVuzDotNetCore2/Models/Gradebook/ElGradebook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KisVuzDotNetCore2.Models.Gradebook
+{
+    /// <summary>
+    /// Электронный журнал учета успеваемости обучающихся
+    /// </summary>
+    public class ElGradebook
+    {
+        /// <summary>
+        /// УИД электронного журнала
+        /// </summary>
+        public int ElGradebookId { get; set; }
+
+        /// <summary>
+        /// Наименование дисциплины
+        /// </summary>
+        public string DisciplineName { get; set; }
+
+        /// <summary>
+        /// Учебный год
+        /// </summary>
+        public string EduYear { get; set; }
+
+        /// <summary>
+        /// Номер семестра
+        /// </summary>
+        public int SemesterNumber { get; set; }
+
+        /// <summary>
+        /// Факультет
+        /// </summary>
+        public string Faculty { get; set; }
+
+        /// <summary>
+        /// Кафедра
+        /// </summary>
+        public string Department { get; set; }
+
+        /// <summary>
+        /// Курс
+        /// </summary>
+        public int Course { get; set; }
+
+        /// <summary>
+        /// Наименование учебной группы
+        /// </summary>
+        public string GroupName { get; set; }
+
+        /// <summary>
+        /// УИД учебной группы
+        /// </summary>
+        public int GroupId { get; set; }
+
+        /// <summary>
+        /// Преподаватели
+        /// </summary>
+        public List<ElGradebookTeacher> ElGradebookTeachers { get; set; }
+    }
+}
---
+++ KisVuzDotNetCore2/Models/Gradebook/ElGradebookTeacher.cs
@@ -0,0 +1,32 @@
+﻿namespace KisVuzDotNetCore2.Models.Gradebook
+{
+    /// <summary>
+    /// Преподаватель
+    /// </summary>
+    public class ElGradebookTeacher
+    {
+        /// <summary>
+        /// УИД преподавателя
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// ФИО преподавателя
+        /// </summary>
+        public string TeacherFio { get; set; }
+
+        /// <summary>
+        /// УИД аккаунта преподавателя
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// УИД журнала
+        /// </summary>
+        public int ElGradebookId { get; set; }
+        /// <summary>
+        /// Журнал
+        /// </summary>
+        public ElGradebook ElGradebook { get; set; }
+    }
+}
